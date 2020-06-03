1123092bb5cbd68fa38f750fc633e7d57f3e94db
Yann <mail@it.yann.red>
2020-06-03 14:44:30 +0200

    Affichage des categories de produit

--- src/components/Produits/PizzaMain.js
+++ src/components/Produits/PizzaMain.js
@@ -1,61 +1,48 @@
 import React, { useState, useEffect } from "react";
 import Error from "../Error";
-import { getProduitData } from "../../services/ProduitService";
-import Pizza from "./Pizza";
 
-const PizzaMain  = () => {
-  const [pizza, setPizza] = useState(null);
-  const [error, setError] = useState(null);
+function PizzaMain() {
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [pizzas, setPizzas] = useState([]);
 
-  useEffect(() => {
-    try {
-      console.log("La pizza : ", pizza);
-      //
-      setError(null);
-    } catch (e) {
-      setError({ message: e.message });
+    useEffect(() => {
+        fetch("https://127.0.0.1:8000/api/categorie_produits")
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    setIsLoaded(true);
+                    setPizzas(result['hydra:member']);
+                },
+                (error) => {
+                    setIsLoaded(true);
+                    setError(error);
+                }
+            )
+    }, [])
+
+
+    if (error) {
+        return <div>Erreur : {error.message}</div>;
+    } else if (!isLoaded) {
+        return <div>Chargement...</div>;
+    } else {
+        return (
+            <>
+                <div className="container">
+                    <Error error={error} />
+                    <ul>
+                        {pizzas.map(pizza => (
+                            <li key={pizza.nom}>
+                                <a href={pizza['@id']}>{pizza.nom}</a>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            </>
+        );
     }
-  }, [pizza]);
+}
 
-  useEffect(() => {
-    console.log("Je suis monté !");
-  }, []);
-
-  const getDataApi = async () => {
-    try {
-    const res = await getProduitData(pizza);
-    setPizza(res.data)
-    } catch {
-      setError({
-        message: "Une erreur est survenue pendant la récupération des données"
-      });
-    }
-  
-  // const ajoutProduit = () => {}
-
-  };
-
-    return (
-      <>
-      <div className="container">
-        <Error error={error} />
-        <Pizza
-          nom={getDataApi.nom}
-          description={getDataApi.description}
-          prix={getDataApi.prix}
-          photo={getDataApi.photo}
-          promo={getDataApi.promo}
-
-          // onClickHandler={ajoutProduit}
-          nom='La beauté 4 fromages'
-          description='Délicieuse'
-          prix='15'
-          photo='https://picsum.photos/200'
-          promo='false'
-        />
-      </div>
-      </>
-    );
-  };
 
 export default PizzaMain;
