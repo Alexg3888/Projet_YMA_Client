45dcd632a380d19a9a3d6268728d18fb20ef595d
Yann <mail@it.yann.red>
2020-06-04 20:18:55 +0200

    Ajout d'un service de recherche des produits à partir d'une categorie
     + Renomage correct des variables
     + gestion d'erreur dans le cas d'une recherche de categorie inexistante

--- src/components/Produits/PizzaMain.js
+++ src/components/Produits/PizzaMain.js
@@ -3,35 +3,50 @@ import React, { useState, useEffect } from "react";
 import Error from "../Error";
 import {getCatProduitData} from "../../services/ApiService";
 import Pizza from "./Pizza";
+import {rechercheProduitsParCategorie} from "../../services/pizzaService";
 
 function PizzaMain() {
     const [error, setError] = useState(null);
     const [isLoaded, setIsLoaded] = useState(false);
-    const [categorieProduits, setCategorieProduits] = useState([]);
+    const [pizzas, setPizzas] = useState([]);
+    const NOM_CATEGORIE = 'Pizzas'
 
     useEffect(() => {
         getCatProduitData()
             .then(result => {
-                setCategorieProduits(result.data['hydra:member'][0]['produits']);
-                // console.log(result.data);
-                // console.log(result.data['hydra:member'][0]['produits']);
+                let categorieRecherchee = rechercheProduitsParCategorie(NOM_CATEGORIE, result)
+                if (categorieRecherchee === null) {
+                    let msgError = new Array()
+                    msgError['Error'] = "Catégorie " + NOM_CATEGORIE + " introuvable."
+                    setError ("Ereur : Catégorie " + NOM_CATEGORIE + " introuvable.")
+                } else {
+                    setPizzas(categorieRecherchee['produits']);
+                    // console.log(1, result.data['hydra:member']);
+                    // console.log(2, categorieRecherchee['produits']);
+                    // console.log(result.data['hydra:member'][0]['produits']);
+                }
             })
             .catch((error) => setError(error))
             .finally(() => setIsLoaded(true))
     }, [])
 
     if (error) {
-        return <div>Une erreur est survenue</div>;
+        return (
+            <>
+                <div>Une erreur est survenue</div>
+                { console.log( error)}
+                <Error error={error} />
+
+            </>
+        )
     } else if (!isLoaded) {
         return <div>Chargement...</div>;
     } else {
         return (
             <>
                 <div className="container">
-                    <Error error={error} />
-                    {/*TODO Gérer le gestionnaire d'erreur*/}
                     <ul>
-                    {categorieProduits.map(categorieProduit => (
+                    {pizzas.map(categorieProduit => (
                         <li key={categorieProduit.id}>
                         <Pizza
                             nom = {categorieProduit.nom}
