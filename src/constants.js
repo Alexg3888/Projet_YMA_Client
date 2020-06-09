7622964c38b20c4ebd5aa43705982fe8dc232ee0
Yann <mail@it.yann.red>
2020-06-09 21:56:27 +0200

    Ajout de l'autentification coté client

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,3 @@
 export const API_URL = `${process.env.REACT_APP_SYMFONY_APP_URL}/api`;
 export const API_CATEGORIE_PRODUIT_ENDPOINT = API_URL + '/categorie_produits';
+export const API_LOGIN = API_URL + '/login_check';
--- src/services/ApiService.js
+++ src/services/ApiService.js
@@ -1,8 +1,25 @@
 import Axios from "axios";
-import { API_CATEGORIE_PRODUIT_ENDPOINT } from "../constants";
+import { API_CATEGORIE_PRODUIT_ENDPOINT, API_LOGIN } from "../constants";
 
-export const getCatProduitData = async () => {
-  console.log(API_CATEGORIE_PRODUIT_ENDPOINT)
-  //window.localStorage.setItem('token', 'valeurdutoken')
-  return Axios.get(API_CATEGORIE_PRODUIT_ENDPOINT);
+export const getCatProduitData = () => {
+  return Axios.get(API_CATEGORIE_PRODUIT_ENDPOINT, {headers : {'Authorization' : 'Bearer ' + window.localStorage.token}})
+  .catch(async (e)=>{
+    if (e.response.status == '401'){
+      await login()
+      return Axios.get(API_CATEGORIE_PRODUIT_ENDPOINT, {headers : {'Authorization' : 'Bearer ' + window.localStorage.token}})
+    } else {throw e}
+  })
+
 };
+
+//MEMO : le async de cette fonction sert a attendre le retour de "Axios.post(API_LOGIN, jsonBody)" avant de faire le "window.localStorage.setItem"
+export async function login(email, password){
+  const jsonBody = {
+    "username":"user4auth",
+    "password":"%!password4auth!%"
+  }
+
+
+  const token = (await Axios.post(API_LOGIN, jsonBody)).data.token
+  window.localStorage.setItem('token', token)
+}
