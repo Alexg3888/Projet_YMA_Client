38830439e799241f25ece7e0d384ab36f7670bf9
mickael Yen-pon <mickael_yp@hotmail.fr>
2020-07-23 12:25:30 +0200

    modification card produit

--- src/components/Produits/ProduitCard.js
+++ src/components/Produits/ProduitCard.js
@@ -9,11 +9,13 @@ const ProduitCard = (props) => {
                 <div className="card my-3">
                     <img className="card-img-top" src={props.photo} alt="Card image cap"/>
                     <div className="card-body">
+                        <div className="shadow p-3 mb-5 bg-white rounded">
                         <h5 className="card-title">{props.nom}</h5>
                         <p className="card-text" style={{minHeight : 120}}>{props.description}</p>
-                        <p className="card-text"><small className="text-muted">{props.prix} €</small>
-                            <button type="button" className="btn btn-secondary btn-lg btn-block" onClick={()=>(ajouterProduitAuPanier(props.id))}>Ajouter au panier
-                            </button></p>
+                        <p className="text-muted">{props.prix} €</p>
+                        </div>
+                            <button type="button" className="btn btn-warning  btn-block" onClick={()=>(ajouterProduitAuPanier(props.id))}>Ajouter au panier
+                            </button>
                     </div>
                 </div>
             </div>
