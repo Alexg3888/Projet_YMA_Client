f8bc2d77f31f20f367f16d629112108995a752ce
agaudin <agaudin@DEV-SECOIA02>
2020-07-24 12:23:53 +0200

    modal ajouter simplifiée

--- src/components/Produits/ProduitCard.js
+++ src/components/Produits/ProduitCard.js
@@ -1,5 +1,6 @@
 import React from "react";
 import {ajouterProduitAuPanier} from "../../services/PanierService";
+import ModalProduit from "./ModalProduit";
 
 const ProduitCard = (props) => {
 
@@ -19,22 +20,7 @@ const ProduitCard = (props) => {
                     </div>
                 </div>
             </div>
-
-            <div class="modal fade" id="modal" tabindex="-1" role="dialog" aria-labelledby="modalLabel" aria-hidden="true">
-                <div class="modal-dialog">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title" id="modalLabel">Panier</h5>
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                            </button>
-                        </div>
-                        <div class="modal-body">
-                            Vous avez bien ajouté un {props.nom} au panier
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <ModalProduit/>
         </>
     );
 };
