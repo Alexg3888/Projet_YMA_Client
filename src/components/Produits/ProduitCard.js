560259cb716a813b44f02148a8c675e8d26d329a
agaudin <agaudin@DEV-SECOIA02>
2020-07-24 11:52:13 +0200

    modal ajouter

--- src/components/Produits/ProduitCard.js
+++ src/components/Produits/ProduitCard.js
@@ -14,8 +14,24 @@ const ProduitCard = (props) => {
                         <p className="card-text" style={{minHeight : 120}}>{props.description}</p>
                         <p className="text-muted">{props.prix} €</p>
                         </div>
-                            <button type="button" className="btn btn-warning  btn-block" onClick={()=>(ajouterProduitAuPanier(props.id))}>Ajouter au panier
+                            <button type="button" className="btn btn-warning  btn-block" data-toggle="modal" data-target="#modal" onClick={()=>(ajouterProduitAuPanier(props.id))}>Ajouter au panier
                             </button>
+                    </div>
+                </div>
+            </div>
+
+            <div class="modal fade" id="modal" tabindex="-1" role="dialog" aria-labelledby="modalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="modalLabel">Panier</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            Vous avez bien ajouté un {props.nom} au panier
+                        </div>
                     </div>
                 </div>
             </div>
