f4c031956a20fd521072d929b268b671552322d9
mickael Yen-pon <mickael_yp@hotmail.fr>
2020-08-25 11:25:32 +0200

    Modification footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,14 +6,14 @@ function Footer() {
     <footer id="sticky-footer" className="py-4 mt-5 bg-dark text-white">
         <div className="container">
             <div className="row">
-                <div className="col-sm">
+                <div className="col-sm mt-3">
                     <small>Copyright &copy; Bristrot House 2020</small>
                 </div>
                  <div className="col-sm d-flex flex-wrap align-content-center justify-content-center">
                     <img src="/HomePage/logo_nav_blanc.png" className="d-block"></img>                                        
                 </div>
                 <div className="col-sm text-right">
-                    <Link className='nav-link' to="/contact">Contact</Link>
+                    <Link className='nav-link mt-2 text-light' to="/contact">Contact</Link>
                 </div>
             </div>
         </div>
