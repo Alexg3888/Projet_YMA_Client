bf4976ea9539d17fd4c8f42e1a38be9a764577ef
agaudin <agaudin@DEV-SECOIA02>
2020-08-25 13:48:31 +0200

    panier

--- src/components/Panier/PanierLigne.js
+++ src/components/Panier/PanierLigne.js
@@ -4,10 +4,9 @@ function PanierLigne(props) {
 
     return (
         <>
-
-                <div className="col-4 mt-2 mb-1"><img className="miniproduit" src={props.photo} alt="image"/></div>
-                <div className="col-4 mt-3 mb-2">{props.nom}</div>
-                <div className="col-4 mt-3 mb-2">{props.prix} €</div>
+            <div className="col-4 mt-2 mb-1 d-flex flex-wrap align-content-center justify-content-end"><img className="miniproduit" src={props.photo} alt="image"/></div>
+            <div className="col-4 mt-3 mb-2 d-flex flex-wrap align-content-center justify-content-start">{props.nom}</div>
+            <div className="col-4 mt-3 mb-2 d-flex flex-wrap align-content-center justify-content-start">{props.prix} € </div>
         </>
     )
 
--- src/components/Panier/PanierTotal.js
+++ src/components/Panier/PanierTotal.js
@@ -6,7 +6,7 @@ function PanierTotal(props) {
     return (
         <>
             <button type="button" className="btn btn-outline-secondary btn-lg btn-block mt-5" onClick={()=>(supprimerPanier())}>
-                Total : {props.total} €
+                Total : {props.total.toFixed(2)} €
             </button>
         </>
     )
