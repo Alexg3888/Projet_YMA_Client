6990a4ea993d65db5bac1fe969c4d1a8f6c538f3
agaudin <agaudin@DEV-SECOIA02>
2020-08-27 14:02:33 +0200

    ajout lignes page panier

--- src/components/Panier/PanierLigne.js
+++ src/components/Panier/PanierLigne.js
@@ -4,9 +4,9 @@ function PanierLigne(props) {
 
     return (
         <>  
-            <div className="col-4 mt-2 mb-1 d-flex flex-wrap align-content-center justify-content-end"><img className="miniproduit" src={props.photo} alt="image"/></div>
-            <div className="col-4 mt-3 mb-2 d-flex flex-wrap align-content-center justify-content-start">{props.nom}</div>
-            <div className="col-4 mt-3 mb-2 d-flex flex-wrap align-content-center justify-content-start">{props.prix} € </div>
+                <div className="col-4 py-3 d-flex flex-wrap align-content-center justify-content-center border-bottom"><img className="miniproduit" src={props.photo} alt="image"/></div>
+                <div className="col-4 py-3 d-flex flex-wrap align-content-center justify-content-center border-bottom">{props.nom}</div>
+                <div className="col-4 py-3 d-flex flex-wrap align-content-center justify-content-center border-bottom">{props.prix} € </div>
         </>
     )
 
