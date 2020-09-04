b819366191832ebc8a10bb118ec4d073a011c72b
Yann <mail@it.yann.red>
2020-09-04 10:04:49 +0200

    Probleme de l'historique vide réglé

--- src/components/Utilisateur/Historique.js
+++ src/components/Utilisateur/Historique.js
@@ -1,77 +1,80 @@
-import React, { useState, useEffect } from "react";
-import { getHistorique } from "../../services/ApiService";
+import React, {useState, useEffect} from "react";
+import {getHistorique} from "../../services/ApiService";
 import Error from "../Error";
-import { useHistory } from "react-router-dom";
+import {useHistory} from "react-router-dom";
 import Commande from "./Commande";
 
 function Historique(props) {
-  const [error, setError] = useState(null);
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [commandes, setCommandes] = useState([]);
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [commandes, setCommandes] = useState([]);
 
-  const history = useHistory();
+    const history = useHistory();
 
-  useEffect(() => {
-    getHistorique()
-      .then((result) => {
-        console.log(result.data);
-        if (result === null) {
-          alert("Vous n'avez pas encore validé de commande");
-          history.push("/utilisateur");
-        } else {
-          setCommandes(result.data);
-        }
-      })
-      .catch((e) => setError(e))
-      .finally(() => setIsLoaded(true));
-  }, [history]);
+    useEffect(() => {
+        getHistorique()
+            .then((result) => {
+                console.log(result.data);
+                if (result.data === "Pas de commande") {
+                    alert("Vous n'avez pas encore validé de commande");
+                    history.push("/utilisateur");
+                } else {
+                    setCommandes(result.data);
+                }
+            })
+            .catch((e) => setError(e))
+            .finally(() => setIsLoaded(true));
+    }, [history]);
 
-  return (
-    <>
-      {error ? (
+    return (
         <>
-          <div>Une erreur est survenue</div>
-          <Error error={error} />
+            {error ? (
+                <>
+                    <div>Une erreur est survenue</div>
+                    <Error error={error}/>
+                </>
+            ) : (
+                <>
+                    {" "}
+                    {!isLoaded && (
+                        <div class="d-flex justify-content-center">
+                            <div className="spinner-grow text-warning" role="status">
+                                <span className="sr-only">Loading...</span>
+                            </div>
+                        </div>
+                    )}
+                    <div class="container">
+                        <div className="row">
+                            <div
+                                className="col-4 py-3 d-flex flex-wrap align-content-center justify-content-center border-bottom">
+                                Date de commande
+                            </div>
+                            <div
+                                className="col-4 py-3 d-flex flex-wrap align-content-center justify-content-center border-bottom">
+                                Type de commande
+                            </div>
+                            <div
+                                className="col-4 py-3 d-flex flex-wrap align-content-center justify-content-center border-bottom">
+                                Prix total commande
+                            </div>
+                            {/*<div*/}
+                            {/*    className="col-4 py-3 d-flex flex-wrap align-content-center justify-content-center border-bottom">Detail*/}
+                            {/*</div>*/}
+                        </div>
+                        <div className="row">
+                            {commandes.map((commande, index) => (
+                                <Commande
+                                    date_retrait={commande.date_retrait}
+                                    emporte={commande.emporte}
+                                    prix_total={commande.prix_total}
+                                />
+                            ))}
+                        </div>
+                    </div>
+                </>
+            )}
         </>
-      ) : (
-        <>
-          {" "}
-          {!isLoaded && (
-            <div class="d-flex justify-content-center">
-              <div className="spinner-grow text-warning" role="status">
-                <span className="sr-only">Loading...</span>
-              </div>
-            </div>
-          )}
-          <div class="container">
-            <div className="row">
-              <div className="col-4 py-3 d-flex flex-wrap align-content-center justify-content-center border-bottom">
-                Date de commande
-              </div>
-              <div className="col-4 py-3 d-flex flex-wrap align-content-center justify-content-center border-bottom">
-                Type de commande
-              </div>
-              <div className="col-4 py-3 d-flex flex-wrap align-content-center justify-content-center border-bottom">
-                Prix total commande
-              </div>
-              {/*<div*/}
-              {/*    className="col-4 py-3 d-flex flex-wrap align-content-center justify-content-center border-bottom">Detail*/}
-              {/*</div>*/}
-            </div>
-            <div className="row">
-              {commandes.map((commande, index) => (
-                <Commande
-                  date_retrait={commande.date_retrait}
-                  emporte={commande.emporte}
-                  prix_total={commande.prix_total}
-                />
-              ))}
-            </div>
-          </div>
-        </>
-      )}
-    </>
-  );
+    );
 }
 
 export default Historique;
