d57c267db4352ce5f9019bd5ea27a0475aa81296
Yann <mail@it.yann.red>
2020-09-16 10:22:09 +0200

    Supression des warning de la console

--- src/components/Home.js
+++ src/components/Home.js
@@ -185,8 +185,8 @@ function Home() {
       {/* <!-- Adresse + carte --> */}
 
       <div className="container">
-        <div class="row mt-5 ">
-          <div class="col mt-5">
+        <div className="row mt-5 ">
+          <div className="col mt-5">
             <h2 className="mb-4">Bistrot House</h2>
             <h5>Adresse</h5>
             <hr className="my-2" />
@@ -206,15 +206,15 @@ function Home() {
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2783.3565308084662!2d4.855894916201728!3d45.76404462140539!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47f4ea89c07c6663%3A0x33ac6f782adc9be3!2s26%20Boulevard%20Jules%20Favre%2C%2069006%20Lyon!5e0!3m2!1sfr!2sfr!4v1598272714388!5m2!1sfr!2sfr"
               width="530"
               height="450"
-              frameborder="0"
-              allowfullscreen=""
+              frameBorder="0"
+              allowFullScreen=""
               aria-hidden="false"
-              tabindex="0"
+              tabIndex="0"
             ></iframe>
           </div>
         </div>
       </div>
-      {/* <!-- Fin Adresse + carte --> */}
+      {/* <!-- Fin Adresse + carte  --> */}
     </>
   );
 }
