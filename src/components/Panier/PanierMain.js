595f50f3a24c1302bfdd0a066a9277cf15891382
Yann <mail@it.yann.red>
2020-09-18 09:03:40 +0200

    responsive panier

--- src/components/Panier/PanierMain.js
+++ src/components/Panier/PanierMain.js
@@ -14,8 +14,8 @@ function PanierMain(props) {
     return (
         <>
 
-                    {/* <!-- Carousel  --> */}
-                    <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
+            {/* <!-- Carousel  --> */}
+            <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
                 <div className="carousel-inner">
                     <div className="carousel-item active">
                         <img src="/HomePage/slidevotrepanier.jpg" className="d-block w-100" alt="..."/>
@@ -23,37 +23,44 @@ function PanierMain(props) {
                 </div>
             </div>
             {/* <!-- Fin carousel --> */}
-            <div className="container mt-5">
-            
-                <Panier />
+            <div className="container">
+
+                <Panier/>
 
                 {/*TODO YC : Rafraichir le panier une fois celui-ci supprimer*/}
 
                 {/*****Bouton vider le panier*/}
-                <button type="button" className="btn btn-warning mt-5" onClick={()=> {
-                    supprimerPanier()
-                    alert('Votre panier a été vidé')
-                    history.push("/")
-                }}>
-                    Vider le panier
-                </button>
-                {/*****FIN Bouton vider le panier*/}
-                {/*****Bouton valider le panier*/}
-                <button type="button" className="btn btn-warning mt-5 ml-5" onClick={()=>{
-                    let panier = window.localStorage.getItem('panier');
-                    if (panier !== '[]'){
-                        if (!props.loginState){
-                            alert ("Veuillez vous connecter avant de valider la commande")
-                        } else {
-                            history.push("/validationPanier")
-                        }
-                    } else {
-                        alert ("Panier vide, impossible de valider la commande")
-                    }
-                }}>
-                    Valider le panier
-                </button>
-                {/*****FIN Bouton valider le panier*/}
+                <div className="row">
+                    <div className="col-md text-center ml-5">
+                        <button type="button" className="btn btn-warning mt-5" onClick={() => {
+                            supprimerPanier()
+                            alert('Votre panier a été vidé')
+                            history.push("/")
+                        }}>
+                            Vider mon panier
+
+                        </button>
+                        {/*****FIN Bouton vider le panier*/}
+                    </div>
+                    <div className="col text-center">
+                        {/*****Bouton valider le panier*/}
+                        <button type="button" className="btn btn-warning mt-5 ml-5" onClick={() => {
+                            let panier = window.localStorage.getItem('panier');
+                            if (panier !== '[]') {
+                                if (!props.loginState) {
+                                    alert("Veuillez vous connecter avant de valider la commande")
+                                } else {
+                                    history.push("/validationPanier")
+                                }
+                            } else {
+                                alert("Panier vide, impossible de valider la commande")
+                            }
+                        }}>
+                            Valider commande
+                        </button>
+                        {/*****FIN Bouton valider le panier*/}
+                    </div>
+                </div>
             </div>
         </>
     )
